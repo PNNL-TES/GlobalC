2dd37ad29d756ba4a1210c55a9d1edc759eab56b
jinshijian <jianjinshi734@gmail.com>
2019-11-14 15:51:11 -0500

    small changes for function orgnization

--- functions.R
+++ functions.R
@@ -488,6 +488,51 @@ method4 <- function(n, x, s) {    # replace NA errors with zero
   method2(n, x, replace_na(s, 0))
 }
 
+# test sample size and repeat time
+sample_size_tst <- function(test_size, N_SAMPLES) {
+  
+  # resample global soil respiration (RSG)
+  resample_RSG <- tibble(RSG1_no_SD = sapply(rep(test_size, N_SAMPLES), method1, GlobalRs$Rs),
+                         RSG2_miss_SD_median = sapply(rep(test_size, N_SAMPLES), method2, GlobalRs$Rs, GlobalRs$SD),
+                         RSG3_miss_SD_max = sapply(rep(test_size, N_SAMPLES), method3, GlobalRs$Rs, GlobalRs$SD),
+                         RSG4_miss_SD_zero = sapply(rep(test_size, N_SAMPLES), method4, GlobalRs$Rs, GlobalRs$SD)
+  )
+  
+  # comparing RSG resample results
+  resample_RSG %>%
+    gather(Rs_resample_type) %>% 
+    ggplot(aes(value, fill = Rs_resample_type)) +
+    theme_cowplot() +
+    geom_density(stat = 'density', alpha = 0.5) +
+    #  theme(legend.position = c(0.75, 0.75)) +
+    # reverse the color scale so that teal (color 1) is always observations, 
+    # while pink is always implied fluxes
+    scale_fill_discrete("", h.start = 180) + scale_color_discrete(h.start = 180) +
+    ylab("Density") +
+    xlab(expression(R[S]~(Pg~C~yr^{-1}))) -> RSG_sample_fig
+  
+  # resample gross primary production (GPP)
+  resample_GPP <- tibble(GPP1_no_SD = sapply(rep(test_size, N_SAMPLES), method1, GPP$GPP),
+                         GPP2_miss_SD_median = sapply(rep(test_size, N_SAMPLES), method2, GPP$GPP, GPP$SD),
+                         GPP3_miss_SD_max = sapply(rep(test_size, N_SAMPLES), method3, GPP$GPP, GPP$SD),
+                         GPP4_miss_SD_zero = sapply(rep(test_size, N_SAMPLES), method4, GPP$GPP, GPP$SD)
+  )
+  
+  # comparing GPP resample results
+  resample_GPP %>%
+    gather(GPP_resample_type) %>% 
+    ggplot(aes(value, fill = GPP_resample_type)) +
+    theme_cowplot() +
+    geom_density(stat = 'density', alpha = 0.5) +
+    #  theme(legend.position = c(0.75, 0.75)) +
+    # reverse the color scale so that teal (color 1) is always observations, 
+    # while pink is always implied fluxes
+    scale_fill_discrete("", h.start = 180) + scale_color_discrete(h.start = 180) +
+    ylab("Density") +
+    xlab(expression(GPP~(Pg~C~yr^{-1}))) -> GPP_sample_fig
+  
+  plot_grid(RSG_sample_fig, GPP_sample_fig, ncol = 1)
+}
 
 #*****************************************************************************************************************
 # functions not used
