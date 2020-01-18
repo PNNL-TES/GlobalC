c2736c0c95e2060c41079675094904939189174e
Ben Bond-Lamberty <bondlamberty@pnnl.gov>
2020-01-18 14:12:06 -0500

    Report bootstrap distribution overlap
    Implement method in `toyproblem_overlap.Rmd` code by @abigailsnyder see #61

--- functions-stats.R
+++ functions-stats.R
@@ -44,3 +44,85 @@ t_test <- function(x, y, alternative = "two.sided", ...) {
          ", p-value = ", format(z$p.value, digits = 3, scientific = FALSE))
 }
 
+# Calculate overlap between two bootstrap samples
+# This implements the method laid out in `toyproblem_overlap.Rmd`
+calc_overlap <- function(left_sample, right_sample, intersection_interval) {
+  # Test for normality
+  left_normal_test <- shapiro.test(left_sample)
+  left_normal <- left_normal_test$p.value >= 0.05
+  print(left_normal_test)
+  right_normal_test <- shapiro.test(right_sample)
+  right_normal <- right_normal_test$p.value >= 0.05
+  print(right_normal_test)
+  
+  # Calculate mu, sigma, and both normal and non-normal density functions
+  mu_left <- mean(left_sample)
+  sigma_left <- sd(left_sample)
+  mu_right <- mean(right_sample)
+  sigma_right <- sd(right_sample)
+  
+  density_left <- density(left_sample, n = 1024)
+  normal_fn_left <- function(x) dnorm(x, mean = mu_left, sd = sigma_left)
+  numerical_fn_left <- approxfun(x = density_left$x, y = density_left$y)
+  density_right <- density(right_sample, n = 1024)
+  normal_fn_right <- function(x) dnorm(x, mean = mu_right, sd = sigma_right)
+  numerical_fn_right <- approxfun(x = density_right$x, y = density_right$y)
+  
+  # Decide which density functions we're using
+  if(left_normal) {
+    left_dist <- normal_fn_left
+  } else {
+    left_dist <- numerical_fn_left
+  }
+  if(right_normal) {
+    right_dist <- normal_fn_right
+  } else {
+    right_dist <- numerical_fn_right
+  }
+  
+  # Calculate the intersection point
+  intersection <- uniroot(function(x) (left_dist(x) - right_dist(x)), 
+                          interval = intersection_interval)$root
+  cat("Intersection =", intersection, "Pg C/yr\n")
+  
+  # Plot the distributions and intersection
+  tibble(x = seq(min(left_sample) - 50, max(right_sample) + 50, by = 5),
+         left_normal = normal_fn_left(x),
+         right_normal = normal_fn_right(x),
+         left_non_normal = numerical_fn_left(x),
+         right_non_normal = numerical_fn_right(x)) %>%
+    gather(group, density, -x) %>% 
+    ggplot(aes(x, density, color = group)) + geom_line() + 
+    geom_vline(xintercept = intersection, linetype = 2) +
+    annotate("label", x = mu_left, y = 0.01, label = left_normal) +
+    annotate("label", x = mu_right, y = 0.01, label = right_normal) ->
+    p
+  print(p)
+
+  # Calculate percentage of each distribution in overlap region
+  
+  # For vectors of x-axis points `x` and corresponding function evaluation points `y`,
+  # the area under the curve according to the trapezoidal rule is:
+  traprule <- function(x, y) {
+    sum(diff(x) * (head(y, -1) + tail(y, -1)), na.rm = TRUE) / 2
+  }
+  
+  # Left sample: define the x-axis points for integration
+  dx <- 0.01   # integration step size
+  left_xaxis_range <- seq(intersection, 4 * max(left_sample), by = dx ) # 4 is arbitrary
+  # get the corresponding y-axis values
+  left_yaxis_range <- left_dist(left_xaxis_range)
+  # and get the area under the curve
+  left_auc <- traprule(x = left_xaxis_range, y = left_yaxis_range)
+  # Right sample calculated similarly except we can't go below zero, an easy cutoff
+  right_xaxis_range <- seq(0, intersection, by = dx )
+  right_yaxis_range <- right_dist(right_xaxis_range)
+  right_auc <- traprule(x = right_xaxis_range, y = right_yaxis_range)
+  
+  cat("Left sample overlap =", round(left_auc * 100, 1), "%\n")
+  cat("Right sample overlap =", round(right_auc * 100, 1), "%\n")
+  
+  cat("Left quantile of threshold:", ecdf(left_sample)(intersection), "\n")
+  cat("Right quantile of threshold:", ecdf(right_sample)(intersection), "\n")
+}
+
